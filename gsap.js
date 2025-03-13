3366fe56018ad9c4fcbc229584666f49e8c9c844
willian <emai@xgames.com>
2025-03-13 15:15:28 -0300

    layout

--- gsap.js
+++ gsap.js
@@ -7,26 +7,3 @@ hambu.addEventListener("click",()=>{
     nav.classList.toggle("active")
 
 })
-
-let count = 1;
-
-document.getElementById("radio1").checked = true;
-
-
-setInterval(function(){
-
- next()
-
-},2000)
-
-function next (){
-
-    count++;
-
-    if(count > 4){
-        count = 1;
-    }
-
-    document.getElementById("radio"+count).checked = true
-
-}
